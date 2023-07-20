2697516c5d2275f9102aaebedb0e9665f75e58fd
ASTECH <109756054+Astech5@users.noreply.github.com>
2023-07-20 16:49:24 +0100

    first commit

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check 
+// different data types. check the data type of each variable.
+
+console.log(typeof 'Adeshina');
+console.log(typeof 30);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
